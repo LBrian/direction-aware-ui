cae5b38cad970cc6ecd64031cbe7a53f9552266c
Brian Liu <ralphbliu@gmail.com>
2021-02-27 16:32:26 +1100

    Added glob for rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,31 @@
+import copy from 'rollup-plugin-copy'
 import svelte from 'rollup-plugin-svelte';
+import { terser } from "rollup-plugin-terser";
 import resolve from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'src/packages/DaCard/index.svelte',
-  output: {
-    dir: 'build/dist/packages/DaCard',
-    format: 'esm'
-  },
-  plugins: [
-    svelte(require('./svelte.config')),
-    resolve({ browser: true })
-  ]
+const glob = require("glob");
+const packages = glob.sync('src/packages/*/');
+
+function getConfig(target) {
+  const targetDir = `build/dist${target.replaceAll('src', '')}`;
+
+  return {
+    input: `${target}index.svelte`,
+    output: {
+      dir: targetDir,
+      format: 'esm'
+    },
+    plugins: [
+      svelte(require('./svelte.config')),
+      resolve({ browser: true }),
+      terser(),
+      copy({
+        targets: [
+          { src: `${target}index.d.ts`, dest: targetDir }
+        ]
+      })
+    ]
+  }
 }
+
+export default packages.map(path => getConfig(path))
---
+++ src/packages/DaCard/index.d.ts
@@ -0,0 +1,36 @@
+interface DaCard {
+  title?: string;
+  description?: string;
+  color?: string;
+  bgColor?: string;
+  rotateX?: number;
+  rotateY?: number;
+  width?: string;
+  perspective?: string;
+  avatarBrdWidth?: string;
+  avatarBrdColor?: string;
+  avatarAlt?: string;
+  avatarSrc?: string;
+  mediaSrc?: string;
+  mediaAlt?: string;
+  mediaPlaceholderRes?: string;
+  layout?: "figure";
+}
+
+declare namespace global.JSX {
+  interface IntrinsicElements {
+    "da-card": DaCard;
+  }
+}
+
+declare namespace preact.JSX {
+  interface IntrinsicElements {
+    "da-card": DaCard;
+  }
+}
+
+declare namespace svelte.JSX {
+  interface IntrinsicElements {
+    "da-card": DaCard;
+  }
+}
